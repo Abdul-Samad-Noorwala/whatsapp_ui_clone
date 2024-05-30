c71cc148b34ae8fe8d0fa7b8eb1069826140d9a7
Abdul Samad <145452813+Abdul-Samad-Noorwala@users.noreply.github.com>
2024-05-30 15:10:05 +0500

    Created Home Screen UI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_ui_clone/screens/call_screen.dart';
+import 'package:whatsapp_ui_clone/screens/chat_screen.dart';
+import 'package:whatsapp_ui_clone/screens/status_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +13,70 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color(0xFF008069),
+          title: const Text(
+            "WhatsApp",
+            style: TextStyle(
+              color: Colors.white,
+              letterSpacing: 1,
+            ),
+          ),
+          elevation: 1,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.camera_alt,
+                color: Colors.white,
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.more_vert,
+                color: Colors.white,
+              ),
+            ),
+          ],
+          bottom: const TabBar(
+            indicatorWeight: 3,
+            labelStyle: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+            splashFactory: NoSplash.splashFactory,
+            tabs: [
+              Tab(
+                text: ('Chat'),
+              ),
+              Tab(
+                text: ('Status'),
+              ),
+              Tab(
+                text: ('Calls'),
+              ),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            ChatScreen(),
+            StatusScreen(),
+            CallScreen(),
+          ],
+        ),
+      ),
+    );
   }
 }

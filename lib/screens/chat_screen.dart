c76b1380619e215bde5a01ec2cd691b9118f0545
Abdul Samad <145452813+Abdul-Samad-Noorwala@users.noreply.github.com>
2024-05-31 16:02:23 +0500

    Completed Chat Screen UI

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -10,6 +10,30 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: 26,
+        shrinkWrap: true,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: CircleAvatar(
+              radius: 20,
+              backgroundImage: AssetImage("images/img.png"),
+            ),
+            title: Text("Abdul Samad Noorwala"),
+            subtitle: Text("Hey! I Am Using Whatsapp"),
+            trailing: Text("12:45 PM"),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Color(0xFF008069),
+        child: Icon(
+          Icons.chat,
+          color: Colors.white,
+        ),
+      ),
+    );
   }
 }
